68ef8824d45bfba3f350069bcb73429255f42e09
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2023-01-07 14:26:02 +0530

    Check

---
+++ src/main/java/com/driver/Service.java
@@ -0,0 +1,73 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
+
+public class Service {
+
+
+    @Autowired
+    Repository repository;
+
+    public void addOrder(Order order)
+    {
+        repository.addOrder(order);
+    }
+
+    public  void addPatner(String patnerid)
+    {
+        repository.addPatner(patnerid);
+    }
+    public Order getOrderById(String orderId)
+    {
+        return  repository.getOrderById(orderId);
+    }
+    public  DeliveryPartner getPartnerById(String id)
+    {
+        return repository.getPartnerById(id);
+    }
+    public int getOrderCountByPartnerId(String id)
+    {
+        return repository.getOrderCountByPartnerId(id);
+    }
+    public void deleteOrderById(String id)
+    {
+        repository.deleteOrderById(id);
+    }
+    public void deletePartnerById(String id)
+    {
+        repository.deletePartnerById(id);
+    }
+    public void  addOrderPartnerPair(String orderid, String patnerId)
+    {
+        repository.addOrderPartnerPair(orderid,patnerId);
+    }
+    public List<String> getOrdersByPartnerId(String id)
+    {
+        return  repository.getOrdersByPartnerId(id);
+    }
+
+    public List<String> getAllOrders()
+    {
+        return repository.getAllOrders();
+    }
+
+    public String getLastDeliveryTimeByPartnerId(String id)
+    {
+        return  repository.getLastDeliveryTimeByPartnerId(id);
+    }
+
+    public int getOrdersLeftAfterGivenTimeByPartnerId(String time, String patnerId)
+    {
+        return repository.getOrdersLeftAfterGivenTimeByPartnerId(time,patnerId);
+    }
+    public int getCountOfUnassignedOrders()
+    {
+        return repository.getCountOfUnassignedOrders();
+    }
+
+}
